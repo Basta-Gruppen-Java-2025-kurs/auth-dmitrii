7f8218cb93cdd766f5c2cac4ef1e88eea23996d3
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-11-18 11:00:30 +0100

    Initial commit

---
+++ src/test/java/se/dimage/authOne/AuthOneApplicationTests.java
@@ -0,0 +1,13 @@
+package se.dimage.authOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
